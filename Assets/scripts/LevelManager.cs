d8d9daa877a2d0be7c5fab75d34084cd841b2476
AlexeiKonev <96721680+AlexeiKonev@users.noreply.github.com>
2022-10-20 15:49:25 +0300

    add bonus system

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-     
+    public static int score;
+    public Text scoreText;
   public  void ReloadLevel()
     {
         SceneManager.LoadScene(0);
+    }
+    private void Start()
+    {
+        scoreText = GameObject.Find("score").GetComponent<Text>();
+    }
+    private void Update()
+    {
+        scoreText.text = score.ToString();
     }
 }
