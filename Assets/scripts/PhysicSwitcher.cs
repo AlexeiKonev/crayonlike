74edfb4dd3fe1d68038cae984773a75dce2bf563
AlexeiKonev <96721680+AlexeiKonev@users.noreply.github.com>
2022-10-20 14:51:01 +0300

    first commit

---
+++ Assets/scripts/PhysicSwitcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicSwitcher : MonoBehaviour
+{
+    public GameObject ball;
+    public Rigidbody2D rb2D;
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+        rb2D = ball.GetComponent<Rigidbody2D>();
+    }
+ public void SwitchOnPhysics()
+    {
+
+        rb2D.bodyType = RigidbodyType2D.Dynamic;
+    }
+    public void SwitchOffPhysics()
+    {
+
+        rb2D.bodyType = RigidbodyType2D.Static;
+    }
+}
